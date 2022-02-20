87ea3a9abd5e8f3de1186fb137091f79575515b2
crackonosh <lukashais@getgoogleoff.me>
2022-02-20 15:48:49 +0100

    update src/index.js with new bot

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import config from './config';
+import { Client, Intents } from 'discord.js';
+import ready from './listeners/ready';
+import interactionCreate from './listeners/interactionCreate';
 
-console.log(config);
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// register listeners
+ready(client);
+interactionCreate(client);
+
+// start bot
+client.login(config.token);
