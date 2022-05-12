06f34a7483bae523c466bbf27b0360698ff68399
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-05-12 21:28:06 +0200

    1.0.13
    Added support for old style commands

---
+++ Bot/Commands/OldStyleCommand.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace ConsoleApp1.Commands;
+
+public class OldStyleCommand : ModuleBase<SocketCommandContext>
+{
+    /*
+     
+    [Command("test")]
+    public async Task Test()
+    {
+        // Reply to the user message.
+        await Context.Message.ReplyAsync("Pong!");
+    } 
+    
+    */
+}
