c688fafec2f3480827de1adc541e791f347d6190
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-05-12 20:38:06 +0200

    1.11.0
    Correction of some small mistakes.

--- Bot/Commands/RulesCommand.cs
+++ Bot/Commands/RulesCommand.cs
@@ -22,11 +22,11 @@ public class RulesCommand : InteractionModuleBase<SocketInteractionContext>
         "#Je striktně zakázáno šíření toxikomanie v jakékoliv formě, tzn. podávání informací o tom, jak pěstovat, prodávat, kupovat, distribuovat a nebo vyrábět substance. Tohle pravidlo se vztahuje i na soukromé zprávy",
         "#Je zakázáno rozesílat jakýkoliv nevhodný obsah mimo NSFW místnosti, stejně tak je striktně zakázáno sířit osobní údaje ostatních členů.",
         "#Vstup na server je zakázán osobám mladším 15 let.",
-        "#Vše, co se píše na serveru je nutné brát s nadsázkou, tudíž žádný obsah nelze použít jako důkazní materiál. Na serveru působí externí Harm Reduction profesionálové, kteří s porušováním zákonů nemají nic společného.",
+        "#Vše, co se píše na serveru je nutné brát s nadsázkou, tudíž žádný obsah nelze použít jako důkazní materiál. Na serveru působí externí Harm Reduction profesionálové, kteří jsou zde přítomní pro snížení rizika užívání drog a šíření osvěty a neručí za jimi neprodukovaný obsah případně se vyskytující na tomto serveru",
         "#Je přísně zakázáno obcházet jakýkoliv trest, ať už pomocí více účtů, proxy či VPN. Pokud má člen jakýkoliv aktivní trest, musí si trest odpykat.",
         "V neposlední řadě se zamyslete nad tím, co píšete, i když to není zmíněné v pravidlech. Vaše nepřiměřené chování může mít negativní dopad na psychiku ostatních členů, včetně posílání rad, které jsou hlouposti, zlehčování některých situací apod.",
         "Jakožto moderátoři si vyhrazujeme právo takové zprávy mazat, případně upozornit na jejich nepravdivé podklady, potrestat vás a také vás ze serveru zabanovat.",
-        "Berte v potaz, že ctíme zákony České a Slovenské republiky, ToS Discordu a tak se vám může reakce může zdát nepřiměřená situaci.",
+        "Berte v potaz, že ctíme zákony České a Slovenské republiky, ToS Discordu a tak se vám reakce může zdát nepřiměřená situaci.",
     };
     
     private EmbedBuilder SimpleEmbedBuilder()
@@ -57,6 +57,6 @@ public class RulesCommand : InteractionModuleBase<SocketInteractionContext>
         await _logger.Log(new LogMessage(LogSeverity.Info, "[Rules], User " + Context.User.Username + " has generated rules.", null));
         await RespondAsync(embed: SimpleEmbedBuilder().Build());
         // In case you wanted to send just a message
-        // await Context.Channel.SendMessageAsync(embed: SimpleEmbedBuilder().Build());
+        //await Context.Channel.SendMessageAsync(embed: SimpleEmbedBuilder().Build());
     }
 }
